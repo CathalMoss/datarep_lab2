2c31de4b9c6db1ab2ca101712269ad8f69095f3a
CathalMoss <g00371316@gmit.ie>
2020-10-07 12:22:41 +0100

    First Commit

---
+++ car.js
@@ -0,0 +1,33 @@
+class Vehicle {
+
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    Information() {
+        console.log(`Make: ${this.make}.`);
+        console.log(`Model: ${this.model}.`);
+        console.log(`Year: ${this.year}.`);
+    }
+}
+
+class Car extends Vehicle {
+
+    constructor(make, model, year, doors) {
+        super(make, model, year);
+        this.doors = doors;
+    }
+
+    Information() {
+        super.Information();
+        console.log(`Doors: ${this.doors}.`);
+    }
+}
+
+let myCar = new Car('VW','Golf', 2010, 5);
+myCar.Information();
+
+//let MyVehicle = new Vehicle = new Vehicle('VW', 'Golf' 2000);
+//myVehicle.Information();
